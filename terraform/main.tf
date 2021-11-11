fa77cefa228dfb0a4f01c96f61c05f18da649bde
g.butyaev <g.butyaev@rt-solar.ru>
2021-11-11 10:48:50 +0300

    added files for hm

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,9 @@
 provider "yandex" {
-  version = "0.35"
-  token     = "var.service_account_key_file"
-  cloud_id  = "var.cloud_id"
-  folder_id = "var.folder_id"
-  zone      = "var.zone"
+  version   = "0.35"
+  token     = var.service_account_key_file
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+  zone      = var.zone
 }
 resource "yandex_compute_instance" "app" {
   name = "reddit-app0"
@@ -16,36 +16,37 @@ resource "yandex_compute_instance" "app" {
   boot_disk {
     initialize_params {
       # Указать id образа созданного в предыдущем домашем задании
-      image_id = "fd8h0307vdvaiuncudbo"
+      image_id = var.image_id
     }
   }
 
   network_interface {
     # Указан id подсети default-ru-central1-a
-    subnet_id = "e9b08ggehid135lepnt5"
+    subnet_id = var.subnet_id
     nat       = true
   }
+
   metadata = {
-  ssh-keys = "ubuntu:${file("/mnt/c/Users/Gleb/appuser.pub")}"
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
   }
-  
+
   connection {
-    type = "ssh"
-    host = yandex_compute_instance.app.network_interface.0.nat_ip_address
-    user = "ubuntu"
+    type  = "ssh"
+    host  = yandex_compute_instance.app.network_interface.0.nat_ip_address
+    user  = "ubuntu"
     agent = false
-  # путь до приватного ключа
-    private_key = file("~/.ssh/appuser")
-    }
-  
+    # путь до приватного ключа
+    private_key = file(var.private_key)
+  }
+
   provisioner "file" {
-    source = "files/puma.service"
+    source      = "files/puma.service"
     destination = "/tmp/puma.service"
   }
   provisioner "remote-exec" {
-  script = "files/deploy.sh"
+    script = "files/deploy.sh"
   }
-  
+
 
 
 }
---
+++ terraform/variables.tf
@@ -0,0 +1,30 @@
+variable cloud_id {
+  description = "cloud"
+}
+variable folder_id {
+  description = "folder"
+}
+variable zone {
+  description = "Zone"
+  # Значение по умолчанию
+  default = "ru-central1-a"
+}
+variable public_key_path {
+  # Описание переменной
+  description = "public key path"
+}
+variable image_id {
+  description = "image"
+}
+variable subnet_id {
+  description = "subnet"
+}
+variable service_account_key_file {
+  description = "token"
+}
+variable private_key {
+  description = "private key path"
+}
+
+
+
